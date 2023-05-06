c8dbc5bcb42cebe2e49210aaf5aa8c3c89cca75d
bujosa <davidbujosa@gmail.com>
2023-05-06 19:22:12 -0400

    feat: create get_access_token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use base64::engine::general_purpose::STANDARD as BASE64;
+use base64::engine::Engine as _;
+use reqwest;
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    name: String,
+    external_urls: ExternalUrls,
+    popularity: u32,
+    #[serde(rename = "type")]
+    type_: String,
+    uri: String,
+}
+
+#[tokio::main]
+async fn main() {
+    get_access_token().await;
+}
+
+async fn get_access_token() {
+    // Load dotenv
+    dotenv::dotenv().ok();
+
+    // Get the Spotify API token from the environment
+    let client_id = env::var("CLIENT_ID").unwrap();
+    let client_secret = env::var("CLIENT_SECRET").unwrap();
+
+    // Build the request
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://accounts.spotify.com/api/token")
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .header(
+            AUTHORIZATION,
+            format!(
+                "Basic {}",
+                BASE64.encode(&format!("{}:{}", client_id, client_secret))
+            ),
+        )
+        .body("grant_type=client_credentials")
+        .send()
+        .await
+        .unwrap();
+
+    // Get the response
+    let body = res.text().await.unwrap();
+    println!("body = {:?}", body);
 }
